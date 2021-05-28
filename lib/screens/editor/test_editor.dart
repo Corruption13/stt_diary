85e44dfd258ee1751bd300ac812cf5b9146272c3
Sandeep <sandeep.pillai42@gmail.com>
2021-05-28 20:58:06 +0530

    qwer

--- lib/screens/editor/test_editor.dart
+++ lib/screens/editor/test_editor.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:html_editor/html_editor.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:webview_flutter/webview_flutter.dart';
 
 class TestEditor extends StatefulWidget {
 
@@ -10,55 +13,135 @@ class TestEditor extends StatefulWidget {
 }
 
 class _TestEditor extends State<TestEditor> {
-  stt.SpeechToText speech = stt.SpeechToText();
 
   String _counter = "Test";
+  String _speechText = "";
   GlobalKey<HtmlEditorState> keyEditor = GlobalKey();
+  bool isListening = false;
+
   void _debugPrint() async{
     final txt = await keyEditor.currentState.getText();
 
     setState(() {
-      _counter = txt;
+      if(txt.length>100)
+        _counter = txt.substring(0, 100);
+      else
+        _counter = txt;
     });
   }
 
   void _debugSTT() async{
     final txt = await keyEditor.currentState.getText();
+    stt.SpeechToText speech = stt.SpeechToText();
+    void status(String s){
+      print(s);
 
-    setState(() {
-      _counter = txt;
-    });
+    }
+    void result(SpeechRecognitionResult x){
+      print(x);
+      setState(() {
+        _speechText = x.recognizedWords.toString();
+      });
+    }
+    void error(SpeechRecognitionError e){
+      print(e);
+    }
+    isListening = !isListening;
+
+    bool available = await speech.initialize( onStatus: status, onError: error);
+    if ( available || isListening) {
+      speech.listen( onResult: result );
+      String data = _speechText;
+      WebViewController _controller;
+
+      String txt = data
+          .replaceAll("'", '\\"')
+          .replaceAll('"', '\\"')
+          .replaceAll("[", "\\[")
+          .replaceAll("]", "\\]")
+          .replaceAll("\n", "<br/>")
+          .replaceAll("\n\n", "<br/>")
+          .replaceAll("\r", " ")
+          .replaceAll('\r\n', " ");
+
+
+    }
+    else{
+      setState(() {
+        _counter = "Not Listening";
+      });
+    }
+
   }
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
+    return SafeArea(child: Scaffold(
 
-        title: Text("Editor"),
-      ),
-      body: Center(
+        body: Center(
 
-        child: Column(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(_counter),
+                HtmlEditor(
+                  hint: "",
+                  //value: "text content initial, if any",
+                  key: keyEditor,
+                  height: 700,
+                  decoration: BoxDecoration(color: Colors.white),
+                ),
+              SizedBox(height: 32,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.black),
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_counter),
-            HtmlEditor(
-              hint: "Your text here...",
-              //value: "text content initial, if any",
-              key: keyEditor,
-              height: 400,
+                      ),
+                      onPressed: _debugPrint,
+
+                      child: Icon(Icons.save),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 75,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.red),
+                      ),
+                      onPressed: _debugSTT,
+
+                      child: Icon(Icons.mic),
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: 50,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.black),
+
+                      ),
+                      onPressed: (){},
+
+                      child: Icon(Icons.save),
+                    ),
+                  ),
+                ],
+              ),
+
+              ],
             ),
-
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _debugPrint,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
